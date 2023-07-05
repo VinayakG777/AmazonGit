c8e749a01f4c6c7bc4de45afc0bb5eaa1c424001
VinayakG777 <vinayakghumare08@gmail.com>
2023-07-05 09:37:40 +0530

    first commit

---
+++ src/pages/ProductPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class ProductPage {
+public void SearchProduct()
+{
+	System.out.println("search product ");
+}
+public void addToCart()
+{
+	System.out.println("addToCart ");
+}
+public void buyProduct()
+{
+	System.out.println(" buyProduct ");
+}
+}
