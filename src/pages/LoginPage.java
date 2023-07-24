e19b1e98b65a80108bb0b0a34c78dbc58c693148
vinayak <vinayak@gmail.com>
2023-07-24 11:14:28 +0530

    username and enterpass method added

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
 public class LoginPage {
-
+	public void enterUsername()
+	{
+		System.out.println("Enter username");
+	}
+	public void enterPassword()
+	{
+		System.out.println("Enter password");
+	}
 }
