e29f919121799a4c0529d897c8e96401c7369233
vinayak <vinayak@gmail.com>
2023-07-24 11:16:34 +0530

    email and mobileno method added

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
 public class LoginPage {
-
+	public void enterEmail()
+	{
+		System.out.println("Enter username");
+	}
+	public void enterMobieNumber()
+	{
+		System.out.println("Enter password");
+	}
 }
